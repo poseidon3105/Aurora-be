0182e2a9490b099ceaa9d8bbba13dcdc98e6edfd
Khanh <kn01234444@gmail.com>
2026-08-20 21:31:05 +0700

    fix: port

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`Test Google Login:  http://localhost:${port}`);
   console.log(`API Documentation:  http://localhost:${port}/api/docs`);
